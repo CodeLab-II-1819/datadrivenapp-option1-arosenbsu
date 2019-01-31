1650e54b53c6876d6bca5bdf4e6cf3a4a0a49e28
Aaron <aaron.rosen17@bathspa.ac.uk>
2019-01-31 13:59:43 +0000

    Final GUI
    Final Iteration of my GUI including my comments

--- gui/src/ofApp.cpp
+++ gui/src/ofApp.cpp
@@ -1,8 +1,10 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup(){//Sets up anything to be pre-loaded into the scene
+	//Loads The logo png image
 	logo.load("Images/Logo.png");
+	//Loads the IndieFlow ttf font
 	customfont.load("IndieFlower.ttf", 40);
 }
 
@@ -12,27 +14,40 @@ void ofApp::update(){
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw(){//draws images/text/shapes onto the scene
+
+	//Sets two colours as variables
 	ofColor colorOne(57, 42, 40);
 	ofColor colorTwo(0, 0, 0);
-
+	//Uses the colour variables to create background variable
 	ofBackgroundGradient(colorOne, colorTwo, OF_GRADIENT_LINEAR);
 
+	//Sets the width of the line
 	ofSetLineWidth(6);
+	//Draws 4 lines to create my outer border
 	ofDrawLine(1910, 990, 1910, 10);
 	ofDrawLine(1910, 10, 10, 10);
 	ofDrawLine(10, 10, 10, 990);
 	ofDrawLine(10, 990, 1910, 990);
-
+	//Draws 4 lines to create my inner text box
 	ofDrawLine(1860, 940, 1860, 250);
-	ofDrawLine(1860, 250, 700, 250);
-	ofDrawLine(700, 250, 700, 940);
-	ofDrawLine(700, 940, 1860, 940);
+	ofDrawLine(1860, 250, 850, 250);
+	ofDrawLine(850, 250, 850, 940);
+	ofDrawLine(850, 940, 1860, 940);
+	//Draws 4 lines to create my exit box
+	ofDrawLine(1650, 100, 1800, 100);
+	ofDrawLine(1800, 100, 1800, 165);
+	ofDrawLine(1800, 165, 1650, 165);
+	ofDrawLine(1650, 165, 1650, 100);
 
+	//Draws logo at the left upper corner
 	logo.draw(-450, -330);
 
-	customfont.drawString("Choose an option to begin sifting.", 740, 150);
-
+	//Drawing tagline as a string with the custom font
+	customfont.drawString("Choose an option to begin sifting.", 600, 150);
+	//Drawing exit box text as a string with the custom font
+	customfont.drawString("Exit", 1690, 150);
+	//Drawing options as strings with the custom font
 	customfont.drawString("1. Total number of Tweets", 40, 346);
 	customfont.drawString("2. Count Tweets discussing Politics", 40, 412);
 	customfont.drawString("3. Count Tweets discussing Money", 40, 478);
--- gui/src/ofApp.h
+++ gui/src/ofApp.h
@@ -3,13 +3,12 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
-	//test
 	public:
-		void setup();
-		void update();
-		void draw();
-		ofTrueTypeFont customfont;
-		ofImage logo;
+		void setup(); //function that lets me set up the scene
+		void update(); //function which lets me edit the scene with each uodate
+		void draw(); //function that lets me draw the things to the scene
+		ofTrueTypeFont customfont; //creates a variable for a ttf file
+		ofImage logo; //creates a variable for an image file
 		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
